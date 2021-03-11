9004dfc5e6552147384d0d0923accde3947908d3
Daniel Schick <schick@mbi-berlin.de>
2021-03-11 11:32:28 +0100

    running version

--- CAENFastPS.py
+++ CAENFastPS.py
@@ -1,124 +1,167 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from tango import AttrWriteType, DevState, DebugIt, DispLevel
+#!/usr/bin/python3 -u
+# coding: utf8
+from tango import AttrWriteType, DevState, DispLevel
 from tango.server import Device, attribute, command, device_property
 
-# import the drivers you will use
+import socket
+from enum import IntEnum
+
+
+class LoopMode(IntEnum):
+    Current = 0
+    Voltage = 1
 
 
 class CAENFastPS(Device):
+    '''CAENFastPS
+
+    This controls a CAEN FastPS Power Supply
+
     '''
-    This docstring should describe your Tango Class and optionally
-    what it depends on (drivers etc).
-    '''
+    IPaddress = device_property(dtype=str)
+    Port = device_property(dtype=int, default_value=10001)
 
-
-# ------ Attributes ------ #
-
-    humidity = attribute(label='Humidity',
+    current = attribute(label='current',
                          dtype=float,
-                         access=AttrWriteType.READ,
-                         doc='Example for an attribute that can only be read.')
+                         access=AttrWriteType.READ_WRITE,
+                         unit='A',)
 
-    # optionally use fget/fset to point to read and write functions.
-    # Default is "read_temperature"/"write_temperature".
-    # Added some optional attribute properties.
-    temperature = attribute(label='Temperature',
-                            fget='get_temperature',
-                            dtype=float,
-                            access=AttrWriteType.READ_WRITE,
-                            display_level=DispLevel.EXPERT,
-                            min_value=-273.15,
-                            min_alarm=-100,
-                            max_alarm=100,
-                            min_warning=-50,
-                            max_warning=50,
-                            unit='C',
-                            format="8.4f",
-                            doc='Attribute that can be read/written.')
+    voltage = attribute(label='voltage',
+                        dtype=float,
+                        access=AttrWriteType.READ_WRITE,
+                        unit='V',)
 
+    power = attribute(label='power',
+                        dtype=float,
+                        access=AttrWriteType.READ,
+                        unit='W',)
 
-# ------ Device Properties ------ #
-    # device_properties will be set once per family-member and usually -
-    # contain serial numbers or a certain port-ID that needs to be set once -
-    # and will not change while the server is running.
+    loop_mode = attribute(label='loop mode',
+                        dtype=LoopMode,
+                        access=AttrWriteType.READ,
+                        doc='mode of the loop control, either constant voltage or constant current',)
 
-    port = device_property(dtype=int, default_value=10000)
+    enabled = attribute(label='enabled',
+                        dtype=bool,
+                        access=AttrWriteType.READ,)
 
-# ------ default functions that are inherited from parent "Device" ------ #
+    fault = attribute(label='fault',
+                        dtype=bool,
+                        access=AttrWriteType.READ,)
+
     def init_device(self):
-        Device.init_device(self)
-        self.info_stream('Connection established')  # prints this line while -
+        super().init_device()
+        #try:
+        self.info_stream('Trying to connect to {:s}:{:d}'.format(self.IPaddress, self.Port))
+        self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 
+                                socket.IPPROTO_TCP)
+        self.con.connect((self.IPaddress, self.Port))
+        self.con.settimeout(5)
+        self.con.setblocking(True)
+        idn = self.write_read('VER')
+        self.info_stream('Connection established for {:s}'.format(idn))
+        #except:
+         #   self.error_stream('Error on initialization!')
+        
         # in logging mode "info" or lower
         self.set_state(DevState.ON)
 
-        # here you could initiate first contact to the hardware (driver)
-
-        self.__temp = 0  # declaring values for the attributes if needed
-        self.__humid = 0
-
     def delete_device(self):
         self.set_state(DevState.OFF)
-        self.error_stream('A device was deleted!')  # prints this line while -
-        # in logging mode "error" or lower.
-
-    # define what is executed when Tango checks for the state.
-    # Here you could inquire the state of the hardware and not just -
-    # (as it is in default) of the TDS.
-    # Default returns state but also sets state from ON to ALARM if -
-    # some attribute alarm limits are surpassed.
-    def dev_state(self):
-        # possible pseudo code:
-        # if hardware-state and TDS-state is ON:
-        #   return DevState.ON
-        # else:
-        #   return DevState.FAULT
-        return DevState
+        self.info_stream('A device was deleted!')
 
     def always_executed_hook(self):
-        # a method that is executed continuously and by default does nothing.
-        # if you want smth done polled/continuously, put it in this method.
-        # check connection to hardware or whether status is acceptable etc.
-        pass
+        status = self.write_read('MST')
+        number, pad, rjust, size, kind = int(status, 16), '0', '>', 32, 'b'
+        bin_status = f'{number:{pad}{rjust}{size}{kind}}'
+        self.debug_stream(bin_status)
+        self.__enabled = bool(int(bin_status[-1]))
+        if self.__enabled:
+            self.set_state(DevState.RUNNING)
+        else:
+            self.set_state(DevState.ON)
+        
+        self.__fault = bool(int(bin_status[-2]))
+        if self.__fault:
+            self.set_state(DevState.FAULT)
 
-# ------ Read/Write functions ------ #
-    def read_humidity(self):  # this is default to read humidity
-        return self.__humid  # returns the value of the "humidity" attr.
+        self.__loop_mode = int(bin_status[-6])
 
-    def get_temperature(self):  # this was set by fget in attribute declaration
-        return self.__temp
+    def read_current(self):
+        return float(self.write_read('MRI'))
 
-    def write_temperature(self, value):
-        # possibly execute some function here to talk to the hardware -
-        # (e.g. set temperature with a thermostat)
-        self.__temp = value  # update the declared server value of the attr.
+    def write_current(self, value):
+        self.write_read('MWI:{:f}'.format(value))
 
-# ------ Internal Methods ------ #
-    # method that works with multiple input parameters only "inside" this code
+    def read_voltage(self):
+        return float(self.write_read('MRV'))
 
-    def internal_method(self, param1, param2):
-        # do something with param1, param2
-        pass
+    def write_voltage(self, value):
+        self.write_read('MWV:{:f}'.format(value))
 
+    def read_power(self):
+        return float(self.write_read('MRW'))
 
-# ------ COMMANDS ------ #
+    def read_loop_mode(self):
+        return self.__loop_mode
 
-    @DebugIt()  # let the execution of this command be logged in debugging mode
-    @command()  # make method executable through the client -
-    # (as opposed to just callable inside this code)
-    def external_method(self, param):
-        # this kind of method only allows one input parameter
-        pass
+    def read_enabled(self):
+        return self.__enabled
 
-    # more examples of externally executable methods
-    @command()
-    def turn_off(self):
-        self.set_state(DevState.OFF)
+    def read_fault(self):
+        return self.__fault
 
-    @command()
-    def turn_on(self):
+    @command
+    def enable(self):
+        self.write_read('MON')
+
+    @command
+    def disable(self):
+        self.write_read('MOFF')
         self.set_state(DevState.ON)
 
+    @command
+    def current_mode(self):
+        self.write_read('LOOP:I')
+
+    @command
+    def voltage_mode(self):
+        self.write_read('LOOP:V')
+
+    @command(dtype_in=str, doc_in='command', dtype_out=str, doc_out='response')
+    def write_read(self, cmd):
+        try:
+            self.con.send('{:s}\r\n'.format(cmd).encode('utf8'))
+            ret = self.con.recv(1024).decode('utf8')
+            while (ret.find('\r\n') == -1):
+                ret += self.con.recv(1024).decode('utf8')
+        except socket.timeout:
+            self.warning_stream('Socket timeout')
+            return [-2, '']
+        except socket.error:
+            self.error_stream('Socket error')
+            return [-2, '']
+        # evaluate the response
+        
+        ret_cmd = '#{:s}'.format(cmd)
+
+        if '#ACK' in ret:
+            # write command acknowledged - nothing to return
+            self.debug_stream('write command acknowledged')
+            return 0
+        elif '#NACK' in ret:
+            # write command not acknowledged - nothing to return
+            i = ret.find(':')
+            self.warn_stream('write command not acknowledged with error code - {:02d}'.format(int(ret[i+1:-2])))
+            return -1
+        elif ret_cmd in ret:
+            # read command acknowledged - return value
+            res = ret[len(ret_cmd)+1:-2]
+            self.debug_stream('{:s} - {:s}'.format(ret_cmd, res))
+            return res
+        else:
+            i = ret.find(':')
+            return ret[i+1:-2]
 
 # start the server
 if __name__ == "__main__":
